1b7d71005f491bf4e7f1f6ed9f382034ed7490f6
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-01-24 15:34:58 -0600

    tweak wording

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Meet goes here.
+          Gather goes here.
         </p>
         <a
           className="App-link"
