012a281d7ce4b0f35141a3cd128d1f3ac057c7bc
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-14 12:27:50 -0600

    construct event component

--- src/Event.js
+++ src/Event.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  render() {
+    const { event } = this.props;
+
+    return (
+      <div className='event'>
+          </div>
+        )}
+  }
 }
 
 export default Event;
