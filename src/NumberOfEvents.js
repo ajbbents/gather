a27565e0bc847318165d2c4a1747f21eda8d460d
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-14 14:43:47 -0600

    update code to satisfy test

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
-  state = { num: 32 };
+  state = { num: 10 };
+
+  changeNum = (value) => {
+    this.setState({ num: value });
+  };
+
+  componentDidMount() {
+    this.setState({ num: this.props.num || 10 });
+  };
 
   render() {
+    const { num } = this.state;
     return (
       <div>
-
+        <label>
+          Number of Events
+          <input
+            className='num'
+            type='number'
+            value={num}
+          >
+          </input>
+        </label>
       </div>
     )
   }
