95e3c3ed05e2587721df49083de8473931a46225
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-22 13:46:48 -0600

    adjust location of code and fix getToken bug

--- src/api.js
+++ src/api.js
@@ -5,8 +5,28 @@
  * The Set will remove all duplicates from the array.
  */
 import { mockData } from "./mock-data";
-import axios from "axios";
+import axios from 'axios';
 import NProgress from 'nprogress';
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://h3dqhxwslk.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
