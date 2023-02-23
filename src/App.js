dfb5085c71c6a006ca64ee0aef428eb8d0f6a558
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-23 14:04:01 -0600

    add number of events to state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import { extractLocations, getEvents } from './api';
 class App extends Component {
   state = {
     events: [],
-    locations: []
+    locations: [],
+    numberOfEvents: 10
   };
 
   updateEvents = (location) => {
