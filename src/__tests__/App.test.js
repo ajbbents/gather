16cef9ef435f24b85ee05ff935d2fb9dbd5eedd9
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-27 13:07:16 -0600

    updates

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from '../App';
+import EventList from '../EventList';
+import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
+import { mockData } from '../mock-data';
+import { extractLocations, getEvents } from '../api';
+
+describe('<App /> component', () => {
+    let AppWrapper;
+    beforeAll(() => {
+        AppWrapper = shallow(<App />);
+    });
+
+    test
+})
