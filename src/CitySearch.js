89a2099631c8dbbfad689149327e25a50b5a99db
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-27 10:33:57 -0600

    update citysearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -8,21 +8,30 @@ class CitySearch extends Component {
   }
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'Could not find the city you are looking for. Please try again.',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
-      showSuggestions: false
+      showSuggestions: false,
+      infoText: ''
     });
-
     this.props.updateEvents(suggestion);
   }
 
